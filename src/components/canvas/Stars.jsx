658f90e1dc65e0bf5142772066fc147454ea145c
Abdullah Ahmed <abdullah.ahmed.pisces.1010@gmail.com>
2026-02-23 13:58:44 +0530

    updated changes and minor tweaks

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,20 +1,15 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import { random } from "maath";
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 })
-  );
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x -= delta / 10;
-      ref.current.rotation.y -= delta / 15;
-    }
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
   });
 
   return (
@@ -22,9 +17,9 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="#f272c8"
+          color='#f272c8'
           size={0.002}
-          sizeAttenuation
+          sizeAttenuation={true}
           depthWrite={false}
         />
       </Points>
